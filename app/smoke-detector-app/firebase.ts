77e2b06ae90cc65af35c937fca87ef2b0790fd64
andrusmario <marioandrus05@gmail.com>
2025-12-28 13:22:20 +0000

    Secure Firebase config and fix Expo app startup

--- app/smoke-detector-app/firebase.ts
+++ app/smoke-detector-app/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdRopegrXjftEoLxe32Lvwc_-6duYtBPM",
-  authDomain: "esp32-smoke-detector.firebaseapp.com",
-  databaseURL:
-    "https://esp32-smoke-detector-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "esp32-smoke-detector",
-  storageBucket: "esp32-smoke-detector.appspot.com",
-  messagingSenderId: "625660646664",
-  appId: "1:625660646664:web:ac08babc31cfe9860f08bd",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DB_URL,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
