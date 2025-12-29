ce0321728cf216a0bc8a5c5c7819e96ad8ff2148
andrusmario <marioandrus05@gmail.com>
2025-12-29 17:52:40 +0000

    Move secrets to ignored secrets file

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,66 @@
-#define MQ2_PIN 34
-#define BUZZER_PIN 25
 #include <Arduino.h>
+#define BUZZER_PIN 25
+#define MQ2_PIN 34
+#define SMOKE_THRESHOLD 900
 
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include <time.h>
+
+#include "secrets.h"
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+bool lastSmokeState = false;
+
+void buzzerOff() { digitalWrite(BUZZER_PIN, HIGH); }
+void buzzerOn() { digitalWrite(BUZZER_PIN, LOW); }
 
 void setup() {
-    pinMode(MQ2_PIN, INPUT);
+    Serial.begin(115200);
+
     pinMode(BUZZER_PIN, OUTPUT);
+    digitalWrite(BUZZER_PIN, LOW);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to Wi-Fi");
+
+    while(WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nWiFi connected");
+
+    config.api_key = API_KEY;
+    config.database_url = DATABASE_URL;
+
+    Firebase.begin(&config, &auth);
+    Firebase.reconnectWiFi(true);
+
+    Serial.println("Firebase connected");
 }
 
 void loop() {
     int sensorValue = analogRead(MQ2_PIN);
+    bool smokeDetected = sensorValue > SMOKE_THRESHOLD;
 
-    if(sensorValue > 500) {
-        digitalWrite(BUZZER_PIN, HIGH);
-    } else {
-        digitalWrite(BUZZER_PIN, LOW);
+    Serial.println(sensorValue);
+    digitalWrite(BUZZER_PIN, smokeDetected ? HIGH : LOW);
+
+    Firebase.RTDB.setBool(&fbdo, "/devices/device1/smoke", smokeDetected);
+    Firebase.RTDB.setInt(&fbdo, "/devices/device1/value", sensorValue);
+    Firebase.RTDB.setString(&fbdo, "/devices/device1/lastUpdated", time(nullptr));
+
+    if(smokeDetected && !lastSmokeState) {
+        FirebaseJson alert;
+        alert.set("deviceId", "device1");
+        alert.set("type", "SMOKE");
+        alert.set("message", "Smoke detected");
+        alert.set("active", true);
+        alert.set("timestamp", time(nullptr));
+        Firebase.RTDB.pushJSON(&fbdo, "/alerts", &alert);
     }
-
-    delay(100);
-    
+    lastSmokeState = smokeDetected;
+    delay(1000);
 }
